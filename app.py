fbe6ec85376bedd5ec4f9de0d428b5e99d72d0ba
Uyai-Abasi <isidoreuyaiabasi@gmail.com>
2025-02-04 13:57:27 +0100

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, jsonify
 from datetime import datetime
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/', methods=['GET'])
 def home():
